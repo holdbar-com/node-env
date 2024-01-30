1527752b8cbec3933807d0de717a71fec9515c82
Lars Daniel Blom Rasmussen <lars@holdbar.com>
2024-01-30 14:29:48 +0100

    feat(templates)

---
+++ template/.prettierrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  singleAttributePerLine: false,
+};
